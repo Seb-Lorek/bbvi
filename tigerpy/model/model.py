6d35ad1af049e447984cff0206f6329c27e80653
Sebastian Lorek <sebastian.lorek@gmx.de>
2023-05-15 18:51:55 +0200

    improve class `Model()`

--- tigerpy/model/model.py
+++ tigerpy/model/model.py
@@ -3,51 +3,168 @@ Model container
 """
 
 import numpy as np
+import inspect
+from scipy.interpolate import BSpline as bs
+from typing import Any, Union
+
+import tensorflow_probability.substrates.jax.distributions as tfjd
+import tensorflow_probability.substrates.numpy.distributions as tfnd
+
+Array = Any
+Dist = Union[tfjd.Distribution, tfnd.Distribution]
+
 
 class Model:
+    """
+    A static model.
+    """
+    def __init__(self, intercept=True, X_copy=True):
+        self.intercept = intercept
+        self.X_copy = X_copy
+        self.loglik = None
+        self.logprior = None
+        self.residuals = None
 
-    def __init__(self, fit_intercept=True, copy_X=True):
-        self.fit_intercept = fit_intercept
-        self.copy_X = copy_X
-        self.coef = None
-        self.latents = None
-
-    # define the observations
-    def Obs(self, data, distribution, name):
-        """Module to store the observations.
+    # define the response
+    def response(self, y: Array, distribution: Dist, invlink: Any = None) -> None:
+        """
+        Method to store the response vector.
 
         Args:
-            data (list(), np.array()): The observed data.
-            distribution (tfp.distributions.Distribution()): A TFP distribution.
-            name (str()): Either "response", "covariates" or "smooth".
+            y (Array): A array containing the response vector.
+            distribution (Dist): A tensorflow probability distribution that defines the response distribution.
+            invlink (Any, optional): Inverse link function that conects the linear predictor to the expectation of the response. Default (None) is the identity link.
         """
 
-        data = np.asarray(data)
+        y = np.asarray(y)
+        self.y = y
+        self.y_dist = distribution
+        self.link = invlink
 
-        if name == "response":
-            self.y = data
-            self.y_dist = distribution
+    # define the fixed covariates
+    def fixed(self, X: Array, intercept=True, X_copy=True) -> None:
+        """
+        Method to define the fixed covariates.
 
-        if name == "covariates" and distribution == None:
-            X = data
+        Args:
+            X (Array): A array that contains the fixed covariates (excluding the intercept).
+            intercept (bool, optional): Should a intercept be included. Defaults to True.
+            X_copy (bool, optional): Should the array be copied? Defaults to True.
+        """
 
-            if self.fit_intercept:
-                X = np.column_stack((np.ones(len(X)), X))
+        # potentially redefine intercept and copy_x
+        self.intercept = intercept
+        self.X_copy = X_copy
 
-            if self.copy_X:
-                self._X = X.copy()
+        if self.intercept:
+            X = np.column_stack((np.ones(len(X)), X))
 
-        if name == "smooth" and distribution == None:
-            X = data
+        if self.X_copy:
+            self.X_fixed = X.copy()
+        else:
+            self.X_fixed = X
 
-            if self.copy_X:
-                self._X = X.copy()
+    # define smooth effects
+    def smooth(self, x: Array, n_knots = 40, degree = 3, rwk = 2) -> None:
+        """
+        Method to define smooth B-spline covariates
 
+        Args:
+            x (Array): Array that contains the the covariate.
+            n_knots (int, optional): Number of Knots . Defaults to 40.
+            degree (int, optional): The degree of the B-spline. Defaults to 3.
+            rwk (int, optional): Random walk order that defines the penalisation of the coefficients. Defaults to 2.
+        """
 
-    # define here the parameters
+        # first don't include an intercept with smooth effects
+        # maybe include later
+        self.intercept = False
 
+        knots = np.linspace(x.min(), x.max(), num=n_knots)
 
-    # define variables/hyperparameters
+        self.X_smooth = bs.design_matrix(x, t=knots, k=degree, extrapolate=True)
+        self.knots_smooth = knots
+        self.order_smooth = rwk
 
+    # define the parameters
+    def param(self, value: Array, distribution: Dist, name: str, group: str) -> None:
+        """
+        Method to define the parameters.
 
-    # build the model
+        Args:
+            value (Array): Inital values for the coefficients.
+            distribution (Dist): A object of class Dist.
+            name(str): Name of the paramter of the response that you want to model. Must be an attribute of the tensorflow probability choosen for the response.
+            type (str): String that defines the group of the parameter either "fixed" or "smooth".
+        """
+        if type(value) is not np.ndarray:
+            value = np.array(value)
+
+        if name in inspect.getfullargspec(self.y_dist).args:
+            setattr(self, name + "_" + group + "_" + "value", value)
+            setattr(self, name + "_" + group + "_" + "distr", distribution)
+        else:
+            print("Error: Please provide a paramter of the response distribution.")
+
+    # compute log-likelihood
+    def log_lik(self) -> Array:
+        """
+        Module for the log-likelihood of the model.
+        Defined as the sum of the log-probabilities of all observed variables
+        with a probability distribution.
+
+        Returns:
+            Array: The log-likelihood of the model
+        """
+
+        # define here the log-likelihood of the model
+        self.loglik = 0 + self.log_prior()
+
+        return self.loglik
+
+    # compute log-prior
+    def log_prior(self) -> Array:
+        """
+        The log-prior of the model.
+
+        Defined as the sum of the log-probabilities of all parameter variables
+        with a probability distribution.
+
+        Returns:
+            Array: The log-prior of the model.
+        """
+
+        # define the log-prior of the model
+        self.logprior = 0
+
+        return self.logprior
+
+    # define hyperparameters currently no latent variables except model coefficients
+    class Var:
+        """
+        Hyperparameters.
+        """
+        def __init__(self, value: Array, name: str = "") -> None:
+            self.value = value
+            self.name = name
+
+        def __repr__(self) -> str:
+            return f'{type(self).__name__}(name="{self.name}")'
+
+    # class to set the distributions of the priors
+    class Dist:
+        """
+        Distribution of the priors
+        """
+
+        def __init__(self, distribution: Dist, name: str = "", *inputs: Any, **kwinputs: Any):
+            self.distribution = distribution
+            self.name = name
+            self.inputs = inputs
+            self.kwinputs = kwinputs
+
+        def init_dist(self):
+            args = [input.value for input in self.inputs]
+            kwargs = {kw: input.value for kw, input in self.kwinputs.items()}
+            dist = self.distribution(*args, **kwargs)
+            return dist
